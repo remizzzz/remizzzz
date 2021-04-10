a7d5d1f46bb7571de5ee2f6cb3c22d5cfe394ba1
Remizov.di@mail.ru <Remizov.di@mail.ru>
2021-04-10 14:01:15 +0300

    Unit test

--- tests/MyLogTest.php
+++ tests/MyLogTest.php
@@ -6,14 +6,38 @@ use Remizov\MyLog;
 class MyLogTest extends TestCase
 {
 
-    public function testLog() {
-        $this->expectOutputString("text\n");
-        MyLog::log("text");
+    function testLogFileContent() {
+        $expectedString = "TEST SOME BULLSHIT";
+        $logTime = time();
+        $logFileDir = __DIR__ . "/logs/$logTime.log";
+
+        MyLog::log($expectedString);
         MyLog::write();
+
+        $logFileContent = file_get_contents($logFileDir);
+
+        $this->assertEquals($expectedString, $logFileContent);
+        $this->expectOutputString($expectedString);
     }
 
-    public function testInstance() {
-        $this->assertInstanceOf(MyLog::class, MyLog::Instance());
+    function testCheckLogFileExist() {
+        MyLog::log(__DIR__);
+
+        $logsCounter = 0;
+
+        foreach (glob(__DIR__ . "/logs/*.log") as $filename) {
+            $logsCounter += 1;
+        }
+
+        MyLog::log($logsCounter);
+
+        MyLog::write();
+
+        foreach (glob(__DIR__ . "/logs/*.log") as $filename) {
+            $logsCounter -= 1;
+        }
+
+        $this->assertEquals(0, $logsCounter);
     }
 	
 }
